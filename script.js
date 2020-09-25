1789c3c0236d71a905e178956a9acfe0b005ca56
Yulia Nesterova <julianstrv@yandex.ru>
2020-09-25 23:28:16 +0300

    feat: добавить функционал сохранения формы

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close-button');
-let popupInputTitle = document.querySelector('.popup__input_title');
-let popupInputSubtitle = document.querySelector('.popup__input_subtitle');
+let nameInput = document.querySelector('.popup__input_title');
+let jobInput = document.querySelector('.popup__input_subtitle');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
+let formElement = document.querySelector('.popup__container');
 
 const togglePopup = function togglePopup() {
   popup.classList.toggle("popup_opened");
 }
 
 const autoFill = function autoFill() {
-  popupInputTitle.value = profileTitle.textContent;
-  popupInputSubtitle.value = profileSubtitle.textContent;
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
 
 
+function formSubmitHandler (evt) {
+  evt.preventDefault();
 
+  nameInput.value;
+  jobInput.value;
+    
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+
+}
 
 editButton.addEventListener("click", togglePopup);
 editButton.addEventListener("click", autoFill);
 popupCloseButton.addEventListener("click", togglePopup);
+formElement.addEventListener('submit', formSubmitHandler);  
+
 
 
