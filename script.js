e7746d20b9360c3a5eca4e3cd7c3a940a0e843d8
Yulia Nesterova <julianstrv@yandex.ru>
2020-09-29 14:40:35 +0300

    feat: форматирование

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let popup = document.querySelector('.popup');
-let editButton = document.querySelector('.profile__edit-button');
-let popupCloseButton = document.querySelector('.popup__close-button');
-let nameInput = document.querySelector('.popup__input_title');
-let jobInput = document.querySelector('.popup__input_subtitle');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let formElement = document.querySelector('.popup__container');
-let submitButton = document.querySelector('.popup__button');
+let popup = document.querySelector(".popup");
+let editButton = document.querySelector(".profile__edit-button");
+let popupCloseButton = document.querySelector(".popup__close-button");
+let nameInput = document.querySelector(".popup__input_title");
+let jobInput = document.querySelector(".popup__input_subtitle");
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
+let formElement = document.querySelector(".popup__container");
+let submitButton = document.querySelector(".popup__button");
 
 const togglePopup = function togglePopup() {
   popup.classList.toggle("popup_opened");
@@ -17,13 +17,12 @@ const autoFill = function autoFill() {
   jobInput.value = profileSubtitle.textContent;
 }
 
-
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
 
   nameInput.value;
   jobInput.value;
-    
+
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
 }
@@ -31,8 +30,5 @@ function formSubmitHandler (evt) {
 editButton.addEventListener("click", togglePopup);
 editButton.addEventListener("click", autoFill);
 popupCloseButton.addEventListener("click", togglePopup);
-formElement.addEventListener('submit', formSubmitHandler); 
-submitButton.addEventListener("click", togglePopup); 
-
-
-
+formElement.addEventListener("submit", formSubmitHandler);
+submitButton.addEventListener("click", togglePopup);
