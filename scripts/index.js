95728c7a5e624210fd957b33b9a03f0e0d35a194
Yulia Nesterova <julianstrv@yandex.ru>
2020-09-29 23:41:40 +0300

    hotfix: исправления работы

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,33 @@
 let popup = document.querySelector(".popup");
 let editButton = document.querySelector(".profile__edit-button");
 let popupCloseButton = document.querySelector(".popup__close-button");
-let nameInput = document.querySelector(".popup__input_title");
-let jobInput = document.querySelector(".popup__input_subtitle");
+let nameInput = document.querySelector(".popup__input_type_title");
+let jobInput = document.querySelector(".popup__input_type_subtitle");
 let profileTitle = document.querySelector(".profile__title");
 let profileSubtitle = document.querySelector(".profile__subtitle");
-let formElement = document.querySelector(".popup__container");
+let formElement = document.querySelector(".popup__form");
 let submitButton = document.querySelector(".popup__button");
 
 const togglePopup = function togglePopup() {
   popup.classList.toggle("popup_opened");
-}
+};
 
 const autoFill = function autoFill() {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
-}
+
+  togglePopup();
+};
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  nameInput.value;
-  jobInput.value;
-
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
+
+  togglePopup();
 }
 
-editButton.addEventListener("click", togglePopup);
 editButton.addEventListener("click", autoFill);
 popupCloseButton.addEventListener("click", togglePopup);
 formElement.addEventListener("submit", formSubmitHandler);
-submitButton.addEventListener("click", togglePopup);
