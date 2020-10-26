ed6b9508c0156d225cf90ba2743cad8997595dea
Yulia Nesterova <julianstrv@yandex.ru>
2020-10-26 23:36:21 +0300

    feat: added popup closing

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const popupEdit = document.querySelector(".popup_type_edit");
 const editButton = document.querySelector(".profile__edit-button");
 const popupCloseButtonEdit = document.querySelector(
-  ".popup__close-button_type_edit"
+    ".popup__close-button_type_edit"
 );
 const nameInput = document.querySelector(".popup__input_type_title");
 const jobInput = document.querySelector(".popup__input_type_subtitle");
@@ -11,136 +11,169 @@ const editFormElement = document.querySelector(".popup__form_type_edit");
 const profileAddButton = document.querySelector(".profile__add-button");
 const popupAdd = document.querySelector(".popup_type_add");
 const popupCloseButtonAdd = document.querySelector(
-  ".popup__close-button_type_add"
+    ".popup__close-button_type_add"
 );
 const cardsContainer = document.querySelector(".elements__items");
 const cardsTemplate = document.querySelector(".cards-template");
 const card = cardsTemplate.content.cloneNode(true);
 const initialCards = [
-  {
-    name: "Ущелье Викос",
-    link:
-      "https://images.unsplash.com/photo-1542322461-4a528f906952?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80",
-  },
-  {
-    name: "Акрополь",
-    link:
-      "https://images.unsplash.com/photo-1599423217192-34da246be9e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80",
-  },
-  {
-    name: "Балос",
-    link:
-      "https://images.unsplash.com/photo-1531169356216-34b7e403b91c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80",
-  },
-  {
-    name: "Метеора",
-    link:
-      "https://images.unsplash.com/photo-1552482496-3c03befc5c25?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-  },
-  {
-    name: "Монемвасия",
-    link:
-      "https://images.unsplash.com/photo-1560454758-babbc0393c24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-  },
-  {
-    name: "Остров Лефкада",
-    link:
-      "https://images.unsplash.com/photo-1599953092601-03fe8d25340d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1485&q=80",
-  },
+    {
+        name: "Ущелье Викос",
+        link:
+            "https://images.unsplash.com/photo-1542322461-4a528f906952?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1952&q=80",
+    },
+    {
+        name: "Акрополь",
+        link:
+            "https://images.unsplash.com/photo-1599423217192-34da246be9e8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80",
+    },
+    {
+        name: "Балос",
+        link:
+            "https://images.unsplash.com/photo-1531169356216-34b7e403b91c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80",
+    },
+    {
+        name: "Метеора",
+        link:
+            "https://images.unsplash.com/photo-1552482496-3c03befc5c25?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+    },
+    {
+        name: "Монемвасия",
+        link:
+            "https://images.unsplash.com/photo-1560454758-babbc0393c24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    },
+    {
+        name: "Остров Лефкада",
+        link:
+            "https://images.unsplash.com/photo-1599953092601-03fe8d25340d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1485&q=80",
+    },
 ];
 const addFormElement = document.querySelector(".popup__form_type_add");
 const descriptionInput = document.querySelector(
-  ".popup__input_type_description"
+    ".popup__input_type_description"
 );
 const linkInput = document.querySelector(".popup__input_type_link");
 const imagePopup = document.querySelector(".popup_type_image");
 const imagePopupCloseButton = document.querySelector(
-  ".popup__close-button_type_image"
+    ".popup__close-button_type_image"
 );
 const imagePopupPicture = document.querySelector(".popup__image");
 const imagePopupCaption = document.querySelector(".popup__caption");
+const popup = Array.from(document.querySelectorAll(".popup"));
+
 
 function togglePopup(popupElement) {
-  popupElement.classList.toggle("popup_opened");
+    popupElement.classList.toggle("popup_opened");
+}
+
+function openPopup(popupElement) {
+    popupElement.classList.add("popup_opened");
+}
+
+function closePopup(popupElement) {
+    popupElement.classList.remove("popup_opened");
 }
 
 function fillAuto() {
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
 
-  togglePopup(popupEdit);
+    openPopup(popupEdit);
 }
 
 function handleEditFormSubmit(evt) {
-  evt.preventDefault();
+    evt.preventDefault();
 
-  profileTitle.textContent = nameInput.value;
-  profileSubtitle.textContent = jobInput.value;
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
 
-  togglePopup(popupEdit);
+    closePopup(popupEdit);
 }
 
 function renderCards() {
-  const cards = initialCards.map((element) => getCard(element));
+    const cards = initialCards.map((element) => getCard(element));
 
-  cardsContainer.append(...cards);
+    cardsContainer.append(...cards);
 }
 
 function handleCardRemove(evt) {
-  evt.target.closest(".element").remove();
+    evt.target.closest(".element").remove();
 }
 
 function toggleLike(element) {
-  element.classList.toggle("element__like-button_clicked");
+    element.classList.toggle("element__like-button_clicked");
 }
 
 function getCard(data) {
-  const card = cardsTemplate.content.cloneNode(true);
+    const card = cardsTemplate.content.cloneNode(true);
 
-  const cardDeleteButton = card.querySelector(".element__delete-button");
-  const cardLikeButton = card.querySelector(".element__like-button");
-  const cardImage = card.querySelector(".element__image");
+    const cardDeleteButton = card.querySelector(".element__delete-button");
+    const cardLikeButton = card.querySelector(".element__like-button");
+    const cardImage = card.querySelector(".element__image");
 
-  card.querySelector(".element__text").innerText = data.name;
-  card.querySelector(".element__image").setAttribute("src", data.link);
-  card.querySelector(".element__image").setAttribute("alt", data.name);
+    card.querySelector(".element__text").innerText = data.name;
+    card.querySelector(".element__image").setAttribute("src", data.link);
+    card.querySelector(".element__image").setAttribute("alt", data.name);
 
-  cardDeleteButton.addEventListener("click", handleCardRemove);
-  cardLikeButton.addEventListener("click", () => toggleLike(cardLikeButton));
+    cardDeleteButton.addEventListener("click", handleCardRemove);
+    cardLikeButton.addEventListener("click", () => toggleLike(cardLikeButton));
 
-  cardImage.addEventListener("click", function () {
-    togglePopup(imagePopup);
+    cardImage.addEventListener("click", function () {
+        togglePopup(imagePopup);
 
-    imagePopupPicture.setAttribute("src", data.link);
-    imagePopupPicture.setAttribute("alt", data.name);
-    imagePopupCaption.textContent = data.name;
-  })
+        imagePopupPicture.setAttribute("src", data.link);
+        imagePopupPicture.setAttribute("alt", data.name);
+        imagePopupCaption.textContent = data.name;
+    })
 
-  return card;
+    return card;
 }
 
 function handleAddFormSubmit(evt) {
-  evt.preventDefault();
+    evt.preventDefault();
 
-  const item = getCard({
-    name: descriptionInput.value,
-    link: linkInput.value,
-  });
+    const item = getCard({
+        name: descriptionInput.value,
+        link: linkInput.value,
+    });
 
-  descriptionInput.value = "";
-  linkInput.value = "";
+    descriptionInput.value = "";
+    linkInput.value = "";
 
-  cardsContainer.prepend(item);
+    cardsContainer.prepend(item);
 
-  togglePopup(popupAdd);
+    closePopup(popupAdd);
 }
 
-editButton.addEventListener("click", fillAuto);
-popupCloseButtonEdit.addEventListener("click", () => togglePopup(popupEdit));
-editFormElement.addEventListener("submit", handleEditFormSubmit);
-profileAddButton.addEventListener("click", () => togglePopup(popupAdd));
-popupCloseButtonAdd.addEventListener("click", () => togglePopup(popupAdd));
-addFormElement.addEventListener("submit", handleAddFormSubmit);
-imagePopupCloseButton.addEventListener("click", () => togglePopup(imagePopup));
+function handlePopupBackgroundClick() {
+    popup.forEach((popupElement) => {
+        popupElement.addEventListener("click", function (evt) {
+            if (evt.target !== evt.currentTarget) {
+                return;
+            }
+            closePopup(popupElement);
+        });
+    })
+}
 
+function handleEscPopup(popupElement) {
+    if (popupElement.classList.contains("popup_opened")) {
+        closePopup(popupElement);
+    }
+}
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.key === "Escape") {
+        popup.forEach((popupElement) => handleEscPopup(popupElement));
+    }
+});
+editButton.addEventListener("click", fillAuto);
+popupCloseButtonEdit.addEventListener("click", () => closePopup(popupEdit));
+editFormElement.addEventListener("submit", handleEditFormSubmit);
+profileAddButton.addEventListener("click", () => openPopup(popupAdd));
+popupCloseButtonAdd.addEventListener("click", () => closePopup(popupAdd));
+addFormElement.addEventListener("submit", handleAddFormSubmit);
+imagePopupCloseButton.addEventListener("click", () => closePopup(imagePopup));
+
+handlePopupBackgroundClick();
 renderCards();
