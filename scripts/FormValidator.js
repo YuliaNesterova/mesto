1f89d0d4f24d5e29f67f67daabd01232ecc6cfe2
Yulia Nesterova <julianstrv@yandex.ru>
2020-11-09 10:34:42 +0300

    feat: добавление классов карточки и валидации

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,11 +1,3 @@
-// const validationParams = {
-//     formSelector: '.popup__form',
-//     inputSelector: '.popup__input',
-//     submitButtonSelector: '.popup__button',
-//     inactiveButtonClass: 'popup__button_inactive',
-//     inputErrorClass: 'popup__input_invalid',
-// };
-
 export class FormValidator {
     constructor(params, formElement) {
         this._params = params;
